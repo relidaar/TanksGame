986096a1ab1cdbe49a8d07b186dbd38a7bf75667
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-11-05 18:21:17 +0200

    Implement algorithms for checking if the point inside a circle or a sector

--- helpers.py
+++ helpers.py
@@ -18,3 +18,18 @@ def to_degrees(value):
 
 def calculate_distance(pos1, pos2):
     return sqrt(abs(pos1[0] - pos2[0]) ** 2 + abs(pos1[1] - pos2[1]) ** 2)
+
+
+def inside_circle(point, center, radius) -> bool:
+    return calculate_distance(point, center) < radius
+
+
+def get_angle(point, center) -> bool:
+    return math.atan2(point[1] - center[1], point[0] - center[0])
+
+
+def inside_sector(point, center, radius, starting, ending) -> bool:
+    angle = to_degrees(get_angle(point, center))
+    first_case = starting < ending and starting < angle < ending
+    second_case = starting > ending and (angle > starting or angle < ending)
+    return inside_circle(point, center, radius) and (first_case or second_case)
