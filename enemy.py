83fc2529152192bd5c9969cd9a2aab636020d40d
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-11-05 12:21:40 +0200

    Add enemy entity

---
+++ enemy.py
@@ -0,0 +1,7 @@
+from tank import Tank
+from game_settings import *
+
+
+class Enemy(Tank):
+    def __init__(self, x, y, movement_speed, rotation_speed, angle=0):
+        super().__init__(IMG_TANK1, x, y, movement_speed, rotation_speed, angle)
