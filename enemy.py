29b99ce36fe7c1493bf0e07572916a1c9b33c0a9
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-11-05 18:22:10 +0200

    Implement checking if player is in enemy's sight

--- enemy.py
+++ enemy.py
@@ -8,6 +8,9 @@ class Enemy(Tank):
     def __init__(self, x, y, angle=0):
         super().__init__(IMG_TANK1, x, y, ENEMY_MOVEMENT_SPEED, ENEMY_ROTATION_SPEED, angle)
 
-    def in_sight(self, player: Player):
-        return True
+    def in_sight(self, player: Player) -> bool:
+        if not player.alive: return False
+        starting = (self.angle - 180 - ENEMY_FOV / 2) % 360
+        ending = (self.angle - 180 + ENEMY_FOV / 2) % 360
+        return inside_sector(player.get_location(), self.get_location(), ENEMY_FOV_DISTANCE, starting, ending)
 
