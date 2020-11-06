98aca807178003918deb2652bf4407d07716a6f8
Oleksandr Sviatetskyi <alex.sviatetskyi@gmail.com>
2020-11-06 12:54:36 +0200

    Update maps

---
+++ maps/Tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Tiles" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="gray-tile.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="white-tile.png"/>
+ </tile>
+</tileset>
